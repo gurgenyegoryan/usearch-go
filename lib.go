14fe52b0046ef669f4ac28b22e177eea282db8bf
Gurgen Yegoryan <21982202+gurgenyegoryan@users.noreply.github.com>
2023-08-10 19:01:33 +0400

    Update lib.go

--- lib.go
+++ lib.go
@@ -6,8 +6,10 @@ import (
 )
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/../c/ -I${SRCDIR}/ -I./
-#cgo LDFLAGS: -L./ -lusearch
+// #cgo CFLAGS: -I${SRCDIR}/../c/ -I${SRCDIR}/ -I./
+// #cgo LDFLAGS: -L./ -lusearch
+#cgo CFLAGS: -I${SRCDIR}/../c/
+#cgo LDFLAGS: -L${SRCDIR}/. -Wl,-rpath,$SRCDIR/. -lusearch_c
 #include <usearch.h>
 #include <stdlib.h>
 */
